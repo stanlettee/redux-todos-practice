ef969810ceb9a7345718675da9e727c742a3d448
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2026-04-25 18:54:49 +0300

    counter todos

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -1,10 +1,14 @@
 import css from "./TaskCounter.module.css";
+import { useSelector } from "react-redux";
+import { activeTodos, completedTodos } from "redux/filter/filterSelectors";
 
 export const TaskCounter = () => {
+  const aTodos = useSelector(activeTodos)
+  const cTodos = useSelector(completedTodos)
   return (
     <div>
-      <p className={css.text}>Active: 0</p>
-      <p className={css.text}>Completed: 0</p>
+      <p className={css.text}>Active: {aTodos.length}</p>
+      <p className={css.text}>Completed: {cTodos.length}</p>
     </div>
   );
 };
