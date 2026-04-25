2e0393789b3583b40b43cd9871e5d4d1c0f10ff1
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2026-04-25 18:41:17 +0300

    filtered todos

--- README.md
+++
@@ -1,1 +0,0 @@
-# redux-todos-practice
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { todosReducer } from "./todos/todosSlice";
-
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
-});
